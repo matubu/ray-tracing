3518a4d6d162ee020fa60c01a4c328b358612c70
Axel Coézard <axel.coezard@gmail.com>
2021-12-21 17:30:21 +0100

    fix parse

--- srcs/parsing/parse.c
+++ srcs/parsing/parse.c
@@ -6,7 +6,7 @@
 /*   By: acoezard <acoezard@student.42nice.f>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/21 13:42:38 by acoezard          #+#    #+#             */
-/*   Updated: 2021/12/21 17:29:21 by acoezard         ###   ########.fr       */
+/*   Updated: 2021/12/21 17:29:59 by acoezard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ t_scene	*parse(int ac, char **av)
 	if (!check_args(ac, av) || !check_file(av[1]))
 		return (NULL);
 	fd = open(av[1], O_RDONLY);
-	scene = scene_init("MINRT", 1080, 720);
+	scene = scene_init("MINIRT", WIDTH, HEIGHT);
 	close(fd);
 	return (scene);
 }
