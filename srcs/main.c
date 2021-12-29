b982419b04b870ec9764dc6e19558d317630778c
Mathias Berger--fruhinsholz <mberger-@c2r9p2.42nice.fr>
2021-12-29 13:27:59 +0100

    autorelauch (file watch)

--- srcs/main.c
+++ srcs/main.c
@@ -6,56 +6,11 @@
 /*   By: mberger- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/16 16:15:51 by mberger-          #+#    #+#             */
-/*   Updated: 2021/12/28 19:44:22 by mberger-         ###   ########.fr       */
+/*   Updated: 2021/12/29 12:07:56 by mberger-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
-/*
-#ifdef DEV_SHOW_NORMAL
-static inline unsigned int	ray_color(const t_vec *orig,
-		const t_vec *ray, const t_scene *scene)
-{
-	t_hit	hit;
-
-	hit.normal = (t_vec){0, 0, 0};
-	if (!ray_scene(orig, ray, scene, &hit))
-		return (0x0);
-	return ((int)(hit.normal.x * 100 + 100)
-			| (int)(hit.normal.y * 100 + 100) << 8
-			| (int)(hit.normal.z * 100 + 100) << 16);
-}
-#elif defined(DEV_SHOW_DISTANCE)
-static int	range(int a, int b, float fac)
-{
-	return (
-		((int)((float)(b & (255 << 0)) * fac
-			+ (float)(a & (255 << 0)) * (1.0 - fac)) & (255 << 0))
-		| ((int)((float)(b & (255 << 8)) * fac
-				+ (float)(a & (255 << 8)) * (1.0 - fac)) & (255 << 8))
-		| ((int)((float)(b & (255 << 16)) * fac
-				+ (float)(a & (255 << 16)) * (1.0 - fac)) & (255 << 16))
-			);
-}
-
-static int	dist(float fac)
-{
-	if (fac < .5)
-		return (range(0x000000, 0x512372, fac * 2.0));
-	else
-		return (range(0x512372, 0xe1536f, fac * 2.0 - 1.0));
-}
-
-static inline unsigned int	ray_scene_color(const t_vec *orig,
-		const t_vec *ray, const t_scene *scene)
-{
-	t_hit	hit;
-
-	if (!ray_scene(orig, ray, scene, &hit))
-		return (0x0);
-	return (dist(1.0 - fmin(hit.dist / 80.0, 1.0)));
-}
-#else*/
 
 static inline float	dist(const t_vec *a, const t_vec *b)
 {
@@ -90,7 +45,6 @@ static inline unsigned int	ray_color(const t_vec *orig,
 	}
 	return (rgbmult(hit.obj->color, 255.0 * fac / scene->lights_count));
 }
-//#endif
 
 typedef struct s_trash
 {
@@ -109,13 +63,12 @@ void	render(const t_scene *scene, const t_window *win,
 {
 	const clock_t	start = clock();
 	// ----- DEBUG -----
-	const t_vec		up = {0, 0, 1};
 	t_trash			t;
 	register int	y;
 	register int	x;
 
 	t.dir = radian_to_vector(&cam->rot_euler);
-	t.cam_right = normalize(cross(&t.dir, &up));
+	t.cam_right = normalize(cross(&t.dir, &((t_vec){0, 0, 1})));
 	t.cam_up = normalize(cross(&t.cam_right, &t.dir));
 	t.half_x = cam->width / 2.0;
 	t.half_y = cam->height / 2.0;
@@ -133,8 +86,7 @@ void	render(const t_scene *scene, const t_window *win,
 	}
 	mlx_put_image_to_window(win->ptr, win->win, win->img, 0, 0);
 	// ----- DEBUG -----
-	printf("rendering took %.3fms\n",
-		(double)(clock() - start) / CLOCKS_PER_SEC * 1000);
+	printf("rendering took %.3fms\n", (double)(clock() - start) / CLOCKS_PER_SEC * 1000);
 }
 
 int	main(int argc, char **argv)
